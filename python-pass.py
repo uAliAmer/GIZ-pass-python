d9d73608c8f3057859720e0e8c9e4a1e952b67ef
ualiamer <ali.amur88@gmail.com>
2021-09-30 19:41:41 +0300

    Done

--- python-pass.py
+++ python-pass.py
@@ -2,6 +2,22 @@
 
 class Solution:
 
-    @staticmethod
-    def longest_palindromic(s: str) -> str:
-        pass
+    def longestPalindrome(self, s: str) -> str:
+        def isPalindrome(s):
+            if s is "":
+                return False
+            for i in range(len(s)//2):
+                if s[i] != s[-1-i]:
+                    return False
+            return True
+        common_subs = {}
+        
+        if s is "":
+            return ""
+            
+        for i in range(len(s)):
+            for j in range(1, len(s)+1):
+                if isPalindrome(s[i:j]):
+                    common_subs[s[i:j]] = len(s[i:j])
+        
+        return max(common_subs, key=common_subs.get)
